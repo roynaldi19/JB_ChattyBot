b3cc968fc201012d22f5c3bb1b61d336db94f244
roynaldi19 <roynaldi19@gmail.com>
2023-06-18 15:15:37 +0700

    stage 2

--- Simple Chatty Bot (Kotlin)/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot (Kotlin)/task/src/bot/SimpleBot.kt
@@ -1,5 +1,12 @@
 package bot
 
 fun main() {
-    // write your code here
+    hello("Aid", 2020)
+
+}
+
+fun hello(name: String, created: Int) {
+    return println("""Hello! my name is $name.
+        |I was created in $created
+    """.trimMargin())
 }
